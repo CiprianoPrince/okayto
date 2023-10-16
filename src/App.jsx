f77364a1eae3a0b0f0b3b7c23d16f413b1f41cd4
Prince <princetonjudecipriano@gmail.com>
2023-10-16 10:51:41 +0800

    fixed routing

--- src/App.jsx
+++ src/App.jsx
@@ -17,18 +17,18 @@ function App() {
         <>
             <Routes>
                 <Route element={<AuthLayout />}>
-                    <Route path="signin" element={<Signin />} />
-                    <Route path="signup" element={<Signup />} />
-                    <Route path="signout" element={<Signout />} />
+                    <Route path="/signin" element={<Signin />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/signout" element={<Signout />} />
                 </Route>
 
                 <Route path="/" element={<ShopLayout />}>
                     <Route index element={<Home />} />
-                    <Route path="shop" element={<Shop />} />
-                    <Route path="most-wanted" element={<MostWanted />} />
-                    <Route path="new-arrival" element={<NewArrival />} />
-                    <Route path="cart" element={<Cart />} />
-                    <Route path="checkout" element={<Checkout />} />
+                    <Route path="/shop" element={<Shop />} />
+                    <Route path="/most-wanted" element={<MostWanted />} />
+                    <Route path="/new-arrival" element={<NewArrival />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/checkout" element={<Checkout />} />
                 </Route>
             </Routes>
         </>
